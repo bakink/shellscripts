4798fa4e0291e75bf838ee56257312a16991b1b8
Baki Sahin <bakisahin@yahoo.com>
2018-04-16 10:50:11 +0300

    Create du_max_dept.sh

---
+++ du_max_dept.sh
@@ -0,0 +1,6 @@
+
+#du -ch . --max-depth=1 
+
+-c, --total produce a grand total
+-h, --human-readable print sizes in human readable format (e.g., 1K 234M 2G)
+--max-depth=, which instructs du to list its subdirectories and their sizes to any desired level of depth (i.e., to any level of subdirectories) in a directory tree
