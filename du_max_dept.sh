fb6c2ba5ef88c6b055172d889ddceb037661997b
Baki Sahin <bakisahin@yahoo.com>
2018-07-31 14:00:26 +0300

    Update du_max_dept.sh

--- du_max_dept.sh
+++ du_max_dept.sh
@@ -4,3 +4,7 @@
 -c, --total produce a grand total
 -h, --human-readable print sizes in human readable format (e.g., 1K 234M 2G)
 --max-depth=, which instructs du to list its subdirectories and their sizes to any desired level of depth (i.e., to any level of subdirectories) in a directory tree
+
+-- find only root fs usage.
+
+#du -k -x / | sort -rn | more
